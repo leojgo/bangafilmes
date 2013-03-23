96140ff52913d93507d94587825d2bc50a3af125
leojgo <leo.go@ig.com.br>
2013-03-23 02:08:59 -0300

    Primeiro commit

---
+++ BangaFilmes/Models/MovieCatalogInitializer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace BangaFilmes.Models
+{
+    public class MovieCatalogInitializer : DropCreateDatabaseIfModelChanges<MovieCatalog>
+    {
+        protected override void Seed(MovieCatalog context)
+        {
+            var filmes = new List<Movie>
+            {
+                new Movie { OriginalTitle="Argo",PosterUrl=@"/capas/Argo.jpg" },
+                new Movie { OriginalTitle="Avatar",PosterUrl=@"/capas/Avtar.jpg" },
+                new Movie { OriginalTitle="Batman",PosterUrl=@"/capas/Batman.jpg" },
+                new Movie { OriginalTitle="Devil",PosterUrl=@"/capas/Devil.jpg" },
+                new Movie { OriginalTitle="Extracted",PosterUrl=@"/capas/Extracted.jpg" }                
+            };
+
+            filmes.ForEach(f => context.Movies.Add(f));
+            context.SaveChanges();
+
+            var generos = new List<Genre>
+            {
+                new Genre { Id=12, Name="Aventura" },
+                new Genre { Id=14, Name="Fantasia" },
+                new Genre { Id=16, Name="Animação" },
+                new Genre { Id=18, Name="Drama" },
+                new Genre { Id=22, Name="Musical" },
+                new Genre { Id=27, Name="Terror" },
+                new Genre {Id=28,Name="Ação" },
+                new Genre {Id=35,Name="Comédia" }
+            };
+
+            generos.ForEach(g => context.Genres.Add(g));
+            context.SaveChanges();
+        }
+    }
+
+}
---
+++ BangaFilmes/Models/Movies.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BangaFilmes.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string ImdbId { get; set; }
+        public string OriginalTitle { get; set; }
+        public string Overview { get; set; }
+        public string PosterUrl { get; set; }        
+        public DateTime? ReleaseDate { get; set; }
+        public string Title { get; set; }
+        public int? Runtime { get; set; }        
+        public double? VoteAverage { get; set; }
+        public int? VoteCount { get; set; }
+
+        public virtual IList<Genre> Genres { get; set; }
+
+        public Movie()
+        {
+            this.Genres = new List<Genre>();
+        }
+    }
+
+
+    public class Genre
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public virtual IList<Movie> Movies {get; private set;}
+    }
+
+    public class MovieCatalog : DbContext
+    {
+        //public MovieCatalog() : base("TesteConnection"){}
+
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Genre>().Property(t => t.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.None);
+        }
+    }
+}

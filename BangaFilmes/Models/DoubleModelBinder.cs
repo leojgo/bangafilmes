2f318036e0d7faa8d033c2c31ff86793692e4825
Leonardo Oliveira <leo.go@ig.com.br>
2013-08-15 13:34:37 -0300

    Apagados os binarios

---
+++ BangaFilmes/Models/DoubleModelBinder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Web.Mvc;
+
+namespace BangaFilmes.Models
+{
+    public class DoubleModelBinder : DefaultModelBinder
+    {
+        public override object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            var valueResult = bindingContext.ValueProvider.GetValue(bindingContext.ModelName);
+            var modelState = new ModelState { Value = valueResult };
+            object actualValue = null;
+            try
+            {
+                actualValue = Convert.ToDouble(valueResult.AttemptedValue, CultureInfo.InvariantCulture);
+            }
+            catch (FormatException e)
+            {
+                modelState.Errors.Add(e);
+            }
+
+            bindingContext.ModelState.Add(bindingContext.ModelName, modelState);
+            return actualValue;
+        }
+
+    }
+}
